2739c45dbbdede35bea6113f24d4522e46d859e6
garystarr-surgi <gary.starr@surgishop.com>
2025-11-24 11:18:13 -0500

    Create hooks.py

---
+++ surgi_roles_access/hooks.py
@@ -0,0 +1,15 @@
+app_name = "surgi_roles_access"
+app_title = "Surgi Roles Access"
+app_publisher = "SurgiShop"
+app_description = "Role-based dashboard and access customizations"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+
+# ------------------------------
+# Override Customer dashboard
+# ------------------------------
+override_doctype_dashboards = {
+    "Customer": "surgi_roles_access.dashboard.customer_dashboard.get_data"
+}
