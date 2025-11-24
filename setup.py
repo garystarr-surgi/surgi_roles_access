0567c4597c42204358902b7a105c44fd4a640026
garystarr-surgi <gary.starr@surgishop.com>
2025-11-24 11:27:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='surgi_roles_access',
-    version='0.0.1',
-    description='Control Connection Shortcuts per Role',
-    author='SurgiShop',
-    author_email='gary.starr@surgishop.com',
+    name="surgi_roles_access",
+    version="0.0.1",
+    description="Role-based dashboard and access customizations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="SurgiShop",
     packages=find_packages(),
+    install_requires=[
+        "frappe"
+    ],
     zip_safe=False,
     include_package_data=True,
-    install_requires=['frappe']
 )
